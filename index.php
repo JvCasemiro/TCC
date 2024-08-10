392b7446efc0e7913d9a9f82b4333e897e2e3dab
Euclides Marques <euclidesmarques270@gmail.com>
2024-08-09 22:09:11 -0300

    Finalizando Login

--- index.php
+++ index.php
@@ -44,19 +44,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <h1 id ="tituloForm">EJ TechHouse</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <div class="container col-md-4 col-sm-6">
-        <h2 class="mb-4">Login</h2>
+        <h2 id ="tituloForm" class="mb-4">Login</h2>
         <?php if ($error): ?>
             <p class="error"><?php echo htmlspecialchars($error); ?></p>
         <?php endif; ?>
         <form action="index.php" method="post" onsubmit="return validateForm()">
             <div class="mb-3">
-                <label for="username" class="form-label">Usuário:</label>
+                <label id ="tituloForm" for="username" class="form-label">Usuário:</label>
                 <input type="text" id="username" name="username" class="form-control">
             </div>
             <div class="mb-3">
-                <label for="password" class="form-label">Senha:</label>
+                <label id ="tituloForm" for="password" class="form-label">Senha:</label>
                 <input type="password" id="password" name="password" class="form-control">
             </div>
             <button type="submit" class="btn btn-primary mt-3">Entrar</button>
