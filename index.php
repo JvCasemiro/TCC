7a67ce4e2b8c623c0daa5ed4d52f0d4660e54f20
João Vitor Casemiro <jv.casemiroo@gmail.com>
2024-08-09 21:11:49 -0300

    Responsividade-Login

--- index.php
+++ index.php
@@ -49,19 +49,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="style.css">
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
-    <div class="login-container">
-        <h2>Login</h2>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <div class="container mt-5">
+        <h2 class="mb-4">Login</h2>
         <?php if ($error): ?>
             <p class="error"><?php echo htmlspecialchars($error); ?></p>
         <?php endif; ?>
         <form action="index.php" method="post" onsubmit="return validateForm()">
-            <label for="username">Usuário:</label>
-            <input type="text" id="username" name="username">
-            <label for="password">Senha:</label>
-            <input type="password" id="password" name="password">
-            <button type="submit">Entrar</button>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Usuário:</label>
+                    <input type="text" id="username" name="username" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Senha:</label>
+                    <input type="password" id="password" name="password" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Entrar</button>
         </form>
     </div>
     <script src="script.js"></script>
