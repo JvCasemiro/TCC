cb52778fd565307872f30dae7d0358acc09c036d
Euclides Marques <euclidesmarques270@gmail.com>
2024-08-13 21:10:35 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root";        
 $password = "";            
-$dbname = "tcc"; 
+$dbname = "TCC"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
