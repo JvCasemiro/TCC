b4a97e7ba9126cf886c702f61db80abcc8012cca
Euclides Marques <euclidesmarques270@gmail.com>
2024-08-16 21:58:00 -0300

    Update crud.php

--- crud.php
+++ crud.php
@@ -143,45 +143,43 @@ $result = $conn->query('SELECT * FROM users');
         </table>
 
         <!-- Formulário para editar usuário -->
-        <?php if (isset($_GET['edit'])): ?>
-            <?php
-            $id = $_GET['edit'];
-            $stmt = $conn->prepare('SELECT * FROM users WHERE id = ?');
-            $stmt->bind_param('i', $id);
-            $stmt->execute();
-            $user = $stmt->get_result()->fetch_assoc();
-            ?>
-            <form method="post" class="mt-5">
-                <h2 id="tituloForm">Editar Usuário</h2>
-                <input type="hidden" name="id" value="<?php echo htmlspecialchars($user['id']); ?>">
-                <div class="mb-3">
-                        <label id="tituloForm" for="username" class="form-label">Usuário:</label>
-                        <div class="input-group">
-                            <span class="input-group-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"></path>
-                                </svg>
-                            </span>
-                            <input type="text" id="username" name="username" class="form-control" placeholder="Digite seu usuário" value="<?php echo isset($_COOKIE['username']) ? htmlspecialchars($_COOKIE['username']) : ''; ?>">
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label id="tituloForm" for="password" class="form-label">Senha:</label>
-                        <div class="input-group">
-                            <span class="input-group-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-                                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"></path>
-                                </svg>
-                            </span>
-                            <input type="password" id="password" name="password" class="form-control" placeholder="Digite sua senha">
-                            <span class="input-group-text" id="togglePassword" style="cursor: pointer;">
-                                <i class="fa fa-eye" id="passwordIcon"></i>
-                            </span>
-                        </div>
-                    </div>
-                <button type="submit" name="edit" class="btn btn-primary">Atualizar</button>
-            </form>
-        <?php endif; ?>
+<?php if (isset($_GET['edit'])): ?><?php $id = $_GET['edit'];
+    $stmt = $conn->prepare('SELECT * FROM users WHERE id = ?');
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
+    $user = $stmt->get_result()->fetch_assoc();
+    $stmt->close();
+    ?>
+    <form method="post"class="mt-5">
+        <h2 id="tituloForm">Editar Usuário</h2>
+        <input type="hidden" name="id" value="<?php echo htmlspecialchars($user['id']); ?>">
+        <div class="mb-3">
+            <label id="tituloForm" for="username" class="form-label">Usuário:</label>
+            <div class="input-group">
+                <span class="input-group-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"></path>
+                    </svg>
+                </span>
+                <input type="text" id="username" name="username" class="form-control" placeholder="Digite seu usuário" value="<?php echo htmlspecialchars($user['username']); ?>">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label id="tituloForm" for="password" class="form-label">Senha:</label>
+            <div class="input-group">
+                <span class="input-group-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1"></path>
+                    </svg>
+                </span>
+                <input type="password" id="password" name="password" class="form-control" placeholder="Digite sua senha" value="<?php echo htmlspecialchars($user['password']); ?>">
+                
+            </div>
+        </div>
+        <button type="submit" name="edit" class="btn btn-primary">Atualizar</button>
+    </form>
+<?php endif; ?>
+
     </div>
     <script src="script.js"></script>
 </body>
