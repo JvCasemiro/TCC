a5c2fd8dacc025c7ec8d02f0c604a00ee1e19d02
Euclides Marques <euclidesmarques270@gmail.com>
2025-08-20 17:45:02 -0300

    fix: Ajuste layout login

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="shortcut icon" href="../assets/img/logo_domx_sem_nome.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/img/logo_domx_sem_nome.png" type="image/x-icon">
     <style>
         .container {
             background-color: #fff;
@@ -140,9 +140,9 @@
     <div class="container" id="container">
         <div class="form-container sign-in-container">
             <div class="logo-container">
-                <img src="../assets/img/logo.png" alt="Logo">
+                <img src="assets/img/logo.png" alt="Logo">
             </div>
-            <form action="../auth/login.php" method="POST" id="loginForm">
+            <form action="auth/login.php" method="POST" id="loginForm">
                 <h1>Acesse sua conta</h1>
                 <p>Entre para gerenciar seu sistema de automação residencial.</p>
                 <div class="input-group">
@@ -152,7 +152,7 @@
                     <input type="password" name="password" id="password" placeholder="Senha" required />
                     <i class="fas fa-eye toggle-password" onclick="togglePassword('password')"></i>
                 </div>
-                <a href="../auth/esqueci_senha.php">Esqueceu sua senha?</a>
+                <a href="auth/esqueci_senha.php">Esqueceu sua senha?</a>
                 <button type="submit">Entrar</button>
             </form>
         </div>
