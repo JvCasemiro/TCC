ab17b742da4bbd3bb3d22862f83b9efb5784a644
Euclides Marques <euclidesmarques270@gmail.com>
2025-10-04 17:15:13 -0300

    fix: Ajuste para ligar led

--- includes/update_light.php
+++ includes/update_light.php
@@ -14,7 +14,6 @@ header('Access-Control-Allow-Headers: Content-Type');
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/php_errors.log');
 
 // Inclui o arquivo de configuração do banco de dados
 require_once __DIR__ . '/../config/database.php';
